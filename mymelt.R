bd9e2bb807e926e93a009c33d471f1c0d57c7e45
stonephoenix <yangjian.sir@gmail.com>
2016-03-29 00:13:22 -0500

    Update mymelt.R

--- mymelt.R
+++ mymelt.R
@@ -2,14 +2,20 @@
 mymelt<-function (M,colname){
   allcol <- colnames(M)
   rest <- setdiff(allcol,colname)
-  re <- NaN
+  re <- data.frame(M[1,colname])
+  colnames(re) <- colname
+  re$var <- 0
+  re$value <- 0
   for (i in rest){
-    part1 <- M[,colname]
-    part1<-cbind(part1,rep(i,dim(M)[1]))
-    part1<-cbind(part1,M[,i])
+    part1 <- data.frame(M[,colname])
+    colnames(part1) <- colname
+    part1<-cbind(part1,var=rep(i,dim(M)[1]))
+    part1$var <- as.character(part1$var)
+    part1<-cbind(part1,value=M[,i])
     re <- rbind(re,part1)
   }
   colnames(re) <- c(colname,"var","value")
+  rownames(re) <- 1:nrow(re)
   re <- re[-1,]
   return (re)
 }
